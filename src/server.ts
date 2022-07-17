e6cced33f3458f422a1ca98b446d586870bede69
Abdullah Adel <abdullahadel.aam@gmail.com>
2022-07-17 13:15:11 +0200

    Added a handler for the root

--- src/server.ts
+++ src/server.ts
@@ -28,8 +28,16 @@ class Server {
 
     private routerConfig() {
         this.app.use("/api/employees", EmployeesRouter);
-    }
 
+        this.app.get("/", (req, res) => {
+            res.send(`
+                <h1>Welcome to Employees API</h1>
+                <p>
+                    Please use <a href='/api/employees'>/api/employees</a> endpoint
+                </p>
+            `);
+        });
+    }
     public start(port: number) {
         return new Promise((resolve, reject) => {
             this.app
